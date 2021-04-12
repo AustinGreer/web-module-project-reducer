90c45a2f8540edf5115e0fca401d65904499b509
Austin Greer <agreer.ag12@gmail.com>
2021-04-12 18:11:50 -0500

    explored application, changed export default reducer to just exporting initial state and reducer, set up useReducer hook where state and dispatch is now in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-import './App.css';
+import {initialState, reducer} from './reducers/index'
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+import './App.css';
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <div className="App">
